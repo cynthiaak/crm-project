cc8ff769098c299208906617d880232cb9e74e22
cynthiaak <cynthia.khalil22@gmail.com>
2025-08-20 13:18:57 +0300

    Adding forgot password page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login.jsx";
 import RMDashboard from "./RMDashboard.jsx"; 
-
+import ForgotPassword from "./ForgotPassword.jsx";
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/rmdashboard" element={<RMDashboard />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
 
   );
