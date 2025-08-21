1d3f5a255f0933ef9cebac85f5ef07362e04b35a
cynthiaak <cynthia.khalil22@gmail.com>
2025-08-21 17:26:06 +0300

    Designing the dashboard

--- frontend/src/RMDashboard.jsx
+++ frontend/src/RMDashboard.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import './RMDashboard.css'
-function RMDashboard(){
-    return (
-        <div>
-            <header class="header">
-                  <h1>RM Dashboard</h1>
-            </header>
+import './RMDashboard.css';
 
-        </div>
-    );
+function RMDashboard() {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <ul>
+          <li>Dashboard</li>
+          <li>Candidates</li>
+          <li>Vacancies</li>
+          <li>Reports</li>
+        </ul>
+      </div>
+
+      <div className="main">
+        <header className="header">
+          <h1>Dashboard</h1>
+          <input
+            type="text"
+            name="query"
+            placeholder="Search candidate, vacancy, etc.." />
+        </header>
+      </div>
+    </div>
+  );
 }
+
 export default RMDashboard;
